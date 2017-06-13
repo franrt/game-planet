c852d5e17224e83f1ba9ddbf097faabc0631e459
vanita22 <vanitayrafita2004@gmail.com>
2017-06-13 03:30:46 -0300

    subiendo primero archivos

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*Funcionamiento del Slider*/
+
+
+
+google_ad_client = "ca-pub-2783044520727903";
+/* jQuery_demo */
+google_ad_slot = "2780937993";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
